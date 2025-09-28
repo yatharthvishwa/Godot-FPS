d8a69bb183c6a5d7c5e14f12cc378d2407f941ef
yatharthvishwa <yatharth4665@gmail.com>
2025-09-29 04:03:22 +0530

    fixed slash

--- slash/slash.gd
+++ slash/slash.gd
@@ -2,9 +2,14 @@ extends Area3D
 
 @export var speed: float = 30.0
 var direction: Vector3 = Vector3.ZERO
+
+func _ready():
+	look_at(global_position + direction, Vector3.UP)
+
 func _process(delta):
 	global_position += direction * speed * delta
-	
+	if direction != Vector3.ZERO:
+		look_at(global_position + -direction, Vector3.UP)
 
 
 func _on_body_entered(body: Node):
